f11b427e714492a5669942659e86e3e3445cd5e9
rammise <105074293+rammise@users.noreply.github.com>
2022-05-09 13:50:31 +0100

    2022-05-09_13-50

--- appsas/src/Navbar.jsx
+++ appsas/src/Navbar.jsx
@@ -1,27 +1,38 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import './pages/Style/NavBar.css';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/blogs">Blogs</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/dashboard">Test Dashboard</Link>
-        </li>
-      </ul>
-    </nav>
+
+          <header className="p-3 bg-dark text-white">
+              <div className="container">
+                <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                  <Link className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"to="/">
+                    HOME
+                  </Link>
+
+                  <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><Link className="nav-link px-2 text-secondary"to="/">Home</Link></li>
+                    <li><Link className="nav-link px-2 text-secondary"to="/blogs">Blogs</Link></li>
+                    <li><Link className="nav-link px-2 text-secondary"to="/contact">Contact</Link></li>
+                    <li><Link className="nav-link px-2 text-secondary"to="/login">Login</Link></li>
+                    <li><Link className="nav-link px-2 text-secondary"to="/dashboard">Test Dashboard</Link></li>
+                  </ul>
+
+                  <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                    <input type="search" className="form-control form-control-dark" placeholder="Search..." aria-label="Search" />
+                  </form>
+
+                  <div className="text-end">
+                    <button type="button" className="btn btn-outline-light me-2">Login</button>
+                    <button type="button" className="btn btn-warning">Sign-up</button>
+                  </div>
+                </div>
+              </div>
+            </header>
+
+
   );
 }
 
