fe05625a468c20d8001ba2282120a927bd4791c3
rammise <105074293+rammise@users.noreply.github.com>
2022-05-09 11:39:16 +0100

    2022-05-09_11-39

--- appsas/src/App.js
+++ appsas/src/App.js
@@ -1,44 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Logo from './logo.svg';
 
 function App() {
 
 
-
-
-  const [manoPerson, setManoPerson] = useState(
-    {
-      Vardas: "Petras",
-      Pavarde:  "Kiulkis",
-      Age: 54
-    }
-  );
-
-  useEffect(()=>{
-    manoPerson.Vardas = 'Lopas'
-    setManoPerson({...manoPerson})
- },[])
-
-
- const handleClick = (e) => {
-  manoPerson.Vardas = 'Avinas';
-  manoPerson.Pavarde = 'Idijotas';
-  manoPerson.Age = 40;
-  setManoPerson({...manoPerson})
-}
-
   return (
-      <div className="card text-center m-3">
-          <h5 className="card-header">Show My Person</h5>
-          <div className="card-body">
-            <ul>
-              <li>Vardas = {manoPerson.Vardas}</li>
-              <li>Pavarde = {manoPerson.Pavarde}</li>
-              <li>Amzius = {manoPerson.Age}</li>
-            </ul>
-          </div>
-          <button onClick={handleClick}>Change Names</button>
-      </div>
+         
+         <main className="form-signin text-center">
+          <form>
+            <img src={Logo} className="mb-4" alt="" width="72" height="57" />
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+        
+            <div className="form-floating">
+              <input type="email" className="form-control" id="floatingInput" name="email" placeholder="name@example.com" />
+              <label for="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+              <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+              <label for="floatingPassword">Password</label>
+            </div>
+        
+            <div className="checkbox mb-3">
+              <label>
+                <input type="checkbox" value="remember-me" /> Remember me
+              </label>
+            </div>
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            <p className="mt-5 mb-3 text-muted">&copy; 2022</p>
+          </form>
+        </main>
+
   );
 
 
